0903df132e76a60cc0757d1d19c6d2e29cd4b20b
mathewfosterut <97989433+mathewfosterut@users.noreply.github.com>
2023-05-10 15:41:48 -0600

    Add files via upload
    Clear Home Angular Assessment for Employment

---
+++ src/app/customer.modal.ts
@@ -0,0 +1,11 @@
+export class Customer {
+
+  constructor(
+    public title?: string,
+    public firstName?: string,
+    public lastName?: string,
+    public email?: string,
+    public acceptTerms?: boolean
+  ) {}
+
+}
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { TitleService } from '../title.service';
+import { Title } from '../title.model';
+import { Customer } from '../customer.modal';
 
 @Component({
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
-  styleUrls: ['./simple-form.component.sass']
+  styleUrls: ['./simple-form.component.sass'],
+  providers: [ TitleService ]
 })
 export class SimpleFormComponent {
+  public titleList: Title[] | undefined
+  public model = new Customer()
+  public clickedInactiveButton = false
+  constructor( private titleService: TitleService) {}
 
+  ngOnInit() {
+    this.titleService.getTitles().subscribe((titles) => {
+      this.titleList = titles.filter( title => title.name !== '!' ).sort((a,b) => a.name > b.name ? 1 : -1)
+      this.model.title = titles.find( title => title.isDefault )?.name
+    })
+  }
+
+  onSubmit(isValid: boolean) {
+    if (isValid && (this.clickedInactiveButton = true) ) {
+      console.log( this.model )
+    } else {
+      this.clickedInactiveButton = true
+    }
+  }
 }
